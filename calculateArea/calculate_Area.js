7b1c6dff6d98b86065c6538bade0df5edf1c584d
LucUAce <acevedolucasuzias@gmail.com>
2025-12-05 18:34:50 -0500

    cone addition!

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,8 @@
 let length;
 let width;
+let radius;
+let lateral;
+const pi=3.141592;
 
 function calculateArea() {
  length = parseFloat(document.getElementById('length').value);
@@ -8,4 +11,13 @@ function calculateArea() {
 let area = length * width;
 
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+function calculateAreaC() {
+ radius = parseFloat(document.getElementById('radius').value);
+ lateral = parseFloat(document.getElementById('lateral').value);
+
+let area = radius * pi*(radius + lateral);
+
+ document.getElementById('resultC').innerText = `The area of the cone is: ${area.toFixed(2)}`;
 }
