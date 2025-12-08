75b8f1230e76e09e87e10c36bee53fa59dd0ddd0
LucUAce <acevedolucasuzias@gmail.com>
2025-12-07 19:27:42 -0500

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,16 +1,17 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad', [4,5,3,4]];
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
          let mainCourseItem = '';
+
 mainCourseMenu.forEach((item, index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
 let dessertItem = '';
-for (let i = 0; i < dessertMenu.length; i++) {
-    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+for (let i = 0; i < dessertMenu.length-1; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}. Price: ${dessertMenu[4][i]}</p>`;}
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
